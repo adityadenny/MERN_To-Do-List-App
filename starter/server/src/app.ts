93cb53ff0fb6451a06771f970cfdf9dda37053b2
adityadenny <adityadenny6@gmail.com>
2022-07-21 17:02:42 +0700

    create route server api sederhana express js

--- starter/server/src/app.ts
+++ starter/server/src/app.ts
@@ -3,4 +3,14 @@ import express, { Request, Response } from 'express'
 const app = express()
 const PORT = 8080
 
-app.get('/', (req: Request, res: Response) => {})
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World')
+})
+
+app.get('/about', (req: Request, res: Response) => {
+    res.send('This is about route')
+})
+
+app.listen(PORT, () => {
+    console.info(`App is listening at http://localhost:${PORT}`)
+}
